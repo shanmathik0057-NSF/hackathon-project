fbdb94868ab16fde3b852914fc33709334688c5e
SowmiyaPS-05 <sowmiya0456@gmail.com>
2026-09-22 22:56:35 +0530

    Create backend API setup

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "BugLens Backend is running"
+    })
+
+@app.route("/events", methods=["POST"])
+def receive_event():
+    data = request.get_json()
+
+    print("Received event:", data)
+
+    return jsonify({
+        "status": "success",
+        "message": "Event received",
+        "event": data
+    }), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
